949c1a82e1a2925c99d693a3a81c783a4a8b8087
Rafael Carvalho <rpaschoal@live.com>
2018-05-13 16:40:09 +1200

    Refactored code to only train the model if the training images have been updated.

--- NZSmartIDReader.Core/NZDriverLicenceSmartIdReader.cs
+++ NZSmartIDReader.Core/NZDriverLicenceSmartIdReader.cs
@@ -14,48 +14,57 @@ namespace NZSmartIDReader.Core
 {
     public sealed class NZDriverLicenceSmartIdReader : SmartIdReader, ISmartIdReader
     {
-        private Project CustomVisionProject { get; set; }
+        private Project _customVisionProject;
+        private Project CustomVisionProject {
+            get
+            {
+                if (_customVisionProject == null)
+                {
+                    var existingProjects = TrainingApi.GetProjects();
+                    var existingProject = existingProjects.FirstOrDefault(p => p.Name.Equals(nameof(NZDriverLicenceSmartIdReader)));
 
-        public NZDriverLicenceSmartIdReader(string samplesDirectory, double predictionThreshold) : base(samplesDirectory, predictionThreshold) { }
+                    if (existingProject == null)
+                    {
+                        _customVisionProject = TrainingApi.CreateProject(nameof(NZDriverLicenceSmartIdReader));
+                    }
+                    else
+                    {
+                        _customVisionProject = existingProject;
+                    }
+                }
+
+                return _customVisionProject;
+            }
+        }
+
+        private TrainingApi TrainingApi { get; set; } = new TrainingApi() { ApiKey = AzureCredentials.TrainingKey };
+
+        public NZDriverLicenceSmartIdReader(string samplesDirectory, double predictionThreshold) : base(samplesDirectory, predictionThreshold) {}
+
+        private (Tag tag1, Tag tag2) GetTags()
+        {
+            var projectTags = TrainingApi.GetTags(CustomVisionProject.Id);
+
+            var tag = projectTags.Tags.FirstOrDefault(t => t.Name.Equals("DriverLicence")) ?? TrainingApi.CreateTag(CustomVisionProject.Id, "DriverLicence");
+            var tag2 = projectTags.Tags.FirstOrDefault(t => t.Name.Equals("NZDriverLicence")) ?? TrainingApi.CreateTag(CustomVisionProject.Id, "NZDriverLicence");
+
+            return (tag, tag2);
+        }
 
         internal override void Train(string samplesDirectory)
         {
-
-            TrainingApi trainingApi = new TrainingApi() { ApiKey = AzureCredentials.TrainingKey };
-
-            Tag tag;
-            Tag tag2;
-
-            var existingProjects = trainingApi.GetProjects();
-
-            CustomVisionProject = existingProjects.FirstOrDefault(p => p.Name.Equals(nameof(NZDriverLicenceSmartIdReader)));
-
-            if (CustomVisionProject == null)
-            {
-                CustomVisionProject = trainingApi.CreateProject(nameof(NZDriverLicenceSmartIdReader));
-
-                // Needs 2 tags otherwise the custom vision API will return BadRequest
-                tag = trainingApi.CreateTag(CustomVisionProject.Id, "DriverLicence");
-                tag2 = trainingApi.CreateTag(CustomVisionProject.Id, "NZDriverLicence");
-            }
-            else
-            {
-                var projectTags = trainingApi.GetTags(CustomVisionProject.Id);
-
-                tag = projectTags.Tags.FirstOrDefault(t => t.Name.Equals("DriverLicence"));
-                tag2 = projectTags.Tags.FirstOrDefault(t => t.Name.Equals("NZDriverLicence"));
-            }
+            var tags = GetTags();
 
             // Batch upload files
             var sampleImagesNZ = Directory.GetFiles(Path.Combine(samplesDirectory, "NewZealand")).Select(img => new ImageFileCreateEntry(Path.GetFileName(img), File.ReadAllBytes(img)));
             var sampleImagesGlobal = Directory.GetFiles(Path.Combine(samplesDirectory, "Global")).Select(img => new ImageFileCreateEntry(Path.GetFileName(img), File.ReadAllBytes(img)));
 
-            trainingApi.CreateImagesFromFiles(CustomVisionProject.Id, new ImageFileCreateBatch(sampleImagesNZ.ToList(), new List<Guid>() { tag.Id, tag2.Id }));
-            trainingApi.CreateImagesFromFiles(CustomVisionProject.Id, new ImageFileCreateBatch(sampleImagesGlobal.ToList(), new List<Guid>() { tag.Id }));
+            TrainingApi.CreateImagesFromFiles(CustomVisionProject.Id, new ImageFileCreateBatch(sampleImagesNZ.ToList(), new List<Guid>() { tags.tag1.Id, tags.tag2.Id }));
+            TrainingApi.CreateImagesFromFiles(CustomVisionProject.Id, new ImageFileCreateBatch(sampleImagesGlobal.ToList(), new List<Guid>() { tags.tag1.Id }));
 
             try
             {
-                var trainingIteration = trainingApi.TrainProject(CustomVisionProject.Id);
+                var trainingIteration = TrainingApi.TrainProject(CustomVisionProject.Id);
 
                 // TODO: Improve this: make the initialization async and add a timeout
                 while (trainingIteration.Status == "Training")
@@ -63,22 +72,15 @@ namespace NZSmartIDReader.Core
                     Thread.Sleep(1000);
 
                     // Re-query the iteration to get it's updated status
-                    trainingIteration = trainingApi.GetIteration(CustomVisionProject.Id, trainingIteration.Id);
+                    trainingIteration = TrainingApi.GetIteration(CustomVisionProject.Id, trainingIteration.Id);
                 }
 
                 trainingIteration.IsDefault = true;
-                trainingApi.UpdateIteration(CustomVisionProject.Id, trainingIteration.Id, trainingIteration);
+                TrainingApi.UpdateIteration(CustomVisionProject.Id, trainingIteration.Id, trainingIteration);
             }
             catch (HttpOperationException e) when (e.Response.Content.Contains("BadRequestTrainingNotNeeded"))
             {
             }
-
-            IsInitialized = true;
-        }
-
-        public bool IsInitialized
-        {
-            get; private set;
         }
 
         public bool IsValid(Stream imageData)
--- NZSmartIDReader.Core/SmartIdReader.cs
+++ NZSmartIDReader.Core/SmartIdReader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ namespace NZSmartIDReader.Core
     public abstract class SmartIdReader
     {
         protected readonly double PredictionThreashold;
+        private readonly string metadataFileName = "TrainingMetaData.ai";
 
         public SmartIdReader(string samplesDirectory, double predictionThreshold)
         {
@@ -19,9 +22,57 @@ namespace NZSmartIDReader.Core
 
             PredictionThreashold = predictionThreshold;
 
-            Train(samplesDirectory);
+            if (NeedsTraining(samplesDirectory))
+            {
+                Train(samplesDirectory);
+                File.WriteAllText(MetadataFullPath(samplesDirectory), GetMostRecentTrainingFileDate(samplesDirectory).ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            }
+
+            IsInitialized = true;
         }
 
         abstract internal void Train(string samplesDirectory);
+
+        public bool IsInitialized
+        {
+            get; private set;
+        }
+
+        private string MetadataFullPath(string samplesDirectory)
+        {
+            return Path.Combine(samplesDirectory, metadataFileName);
+        }
+
+        private DateTime GetMostRecentTrainingFileDate(string samplesDirectory)
+        {
+            var trainingFilesInfo = new DirectoryInfo(samplesDirectory).GetFiles("*", SearchOption.AllDirectories)
+                .Where(f => !f.Name.EndsWith(".ai"))
+                .OrderByDescending(f => f.CreationTimeUtc > f.LastWriteTimeUtc ? f.CreationTimeUtc : f.LastWriteTimeUtc);
+
+            if (trainingFilesInfo.Any())
+            {
+                var mostUpdatedFile = trainingFilesInfo.FirstOrDefault();
+
+                return mostUpdatedFile.CreationTimeUtc > mostUpdatedFile.LastWriteTimeUtc ? mostUpdatedFile.CreationTimeUtc : mostUpdatedFile.LastWriteTimeUtc;
+            }
+
+            return DateTime.MinValue;
+        }
+
+        private bool NeedsTraining(string samplesDirectory)
+        {
+            if (!File.Exists(MetadataFullPath(samplesDirectory)))
+            {
+                return true;
+            }
+            else
+            {
+                var metadata = File.ReadAllText(MetadataFullPath(samplesDirectory));
+
+                var lastSync = Convert.ToDateTime(metadata);
+
+                return Math.Abs((GetMostRecentTrainingFileDate(samplesDirectory) - lastSync).TotalSeconds) >= 1;
+            }
+        }
     }
 }
